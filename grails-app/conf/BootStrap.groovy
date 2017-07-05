9ae0b2784f156f7aa04ab5c5c71d0d7d61b4842f
Fedir kantur <fedichk@gmail.com>
2017-07-05 15:22:58 +0300

    Added startup check for "QN_ID" existing

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import grailstesttask.Counter
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (Counter.findByName("QN_ID") == null) {
+            new Counter(name: "QN_ID", value: 0).save()
+        }
     }
     def destroy = {
     }
